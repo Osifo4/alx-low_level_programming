9bf345a557f60e068de3ef09fff4607ead6fdd52
Osifo4 <osifoemmanuel12@gmail.com>
2022-09-09 16:55:13 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,19 @@
-#!/bin/bash	
-#include <stdio.h>
+#include<stdio.h> 
 
-/**
- * main - Prints a text
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-     printf("with proper grammar, but the outcome is a piece of
-art,\n");
-     return (0);
-}
+ /** 
+  *
+  *  * main -entry point 
+  *
+  *   * Return: Always 0 (sucess) 
+  *
+  *    */ 
+
+ int main(void) 
+
+	 { 
+
+		  printf("with proper grammar, but the outcome is a piece of art,\n"); 
+
+		   return (0); 
+
+		    }
